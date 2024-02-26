1c86acb3dae39bbe77b131d796e2ac60c46e40b5
Steffen Zierold <steffen.zierold@outlook.com>
2024-02-26 16:59:08 +0100

    work on calculating cards+bookmarks

--- js/script.js
+++ js/script.js
@@ -18,13 +18,40 @@ showAnswerComesSoon.forEach((adressAllButtons) => {
 });
 
 // get number of collected Questions (by cards)
-const getNumberOfCards = document.getElementsByClassName("cards");
-const numberOfQuestions = getNumberOfCards.length;
-// for testing:
-console.log(`We collected ${numberOfQuestions} Questions so far.`);
+// Problem: ClassName liest nur die Elemente auf der profile.html aus und dort sind keine Cards, man müsste mit AJAX
+// oder anderem arbeiten >> daher unten stehende Lösung via speichern als Cookie in localStorage
+// const getNumberOfCards = document.getElementsByClassName("cards");
+// const numberOfQuestions = getNumberOfCards.length;
+// // for testing:
+// console.log(`We collected ${numberOfQuestions} Questions so far.`);
 
-const grabcountedQuestions = document.querySelector('[data-js="countedQuestions"]');
-grabcountedQuestions.innerHTML = `We collected ${numberOfQuestions} Questions so far.`;
+// const grabCountedQuestions = document.querySelector('[data-js="countedQuestions"]');
+// grabCountedQuestions.textContent = `We collected ${numberOfQuestions} Questions so far.`;
+
+// Check, ob auf index.html
+if (window.location.pathname === "/index.html" || window.location.pathname === "/") {
+  // Karten zählen (via Array-Länge)
+  const numberOfCards = document.getElementsByClassName("cards").length;
+
+  // Anzahl der "cards" in localStorage speichern
+  localStorage.setItem("numberOfCards", numberOfCards);
+} else if (window.location.pathname === "/profile.html") {
+  // Auf die gespeicherte Anzahl der "cards" zugreifen, während auf profile.html
+  const numberOfCards = localStorage.getItem("numberOfCards");
+
+  // Geben Sie die Anzahl der "cards" auf der Seite aus
+  const grabCountedQuestions = document.querySelector('[data-js="countedQuestions"]');
+  grabCountedQuestions.textContent = `We collected ${numberOfCards} Questions so far.`;
+}
+
+if (window.location.pathname === "/bookmarks.html") {
+  const numberOfBookmarkedCards = document.getElementsByClassName("cards").length;
+  localStorage.setItem("numberOfBookmarkedCards", numberOfBookmarkedCards);
+} else if (window.location.pathname === "/profile.html") {
+  const numberOfBookmarkedCards = localStorage.getItem("numberOfBookmarkedCards");
+  const grabcountedBooksmarks = document.querySelector('[data-js="countedBooksmarks"]');
+  grabcountedBooksmarks.textContent = `There are ${numberOfBookmarkedCards} bookmarked cards so far.`;
+}
 
 // set alert for dark mode Button
 const darkModeButton = document.querySelector('[data-js="modeButton"]');
